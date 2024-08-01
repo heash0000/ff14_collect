ac814b616f06fe922349c7f12579ef77bc55fe6d
heash0000@gmail.com <heash0000@gmail.com>
2024-08-01 13:02:06 +0900

    no message

--- src/router/index.ts
+++ src/router/index.ts
@@ -58,7 +58,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory('/ff14-collect/'),
   routes
 })
 
